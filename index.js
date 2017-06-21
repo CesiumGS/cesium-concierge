22fec2afc33c217cb8cab5c3029851c53b5fde1f
Ottavio Hartman <omh1280@gmail.com>
2017-06-21 13:50:37 -0400

    Pre heroku

--- index.js
+++ index.js
@@ -1,73 +1,71 @@
 'use strict';
 var express = require('express');
-var request = require('request');
+var rp = require('request-promise');
 var githubWebHook = require('express-github-webhook');
 var bodyParser = require('body-parser');
-var chalk = require('chalk');
-var debug = require('debug')('forum-reminder');
+//var chalk = require('chalk');
+//var debug = require('debug')('forum-reminder');
 
-var repos = ['https://github.com/omh1280/cesium'];
-
-var labels = {
-	pr: process.env.GITHUB_PR_LABELS,
-	issue: process.env.GITHUB_ISSUE_LABELS
-};
+//var repos = ['https://github.com/omh1280/cesium'];
 
 var accessToken = process.env.GITHUB_TOKEN;
-var webhookSettings = {
-	path: process.env.WEBHOOK_PATH || '/',
-	secret: process.env.GITHUB_SECRET || 'thisIsASuperSecretSecret'
-};
-
-var webhookHandler = githubWebHook(webhookSettings);
+var webhookHandler = githubWebHook({
+    path: '/',
+    secret: 'secret'
+});
 
 var app = express();
-app.set('port', process.env.PORT || 5555);
+app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(webhookHandler);
 
-webhookHandler.on('pull_request', function (repo, data) {
-	if (repos.indexOf(repo) < 0 || data.action !== 'opened' || labels.pr.length === 0) {return;}
-	debug('[%s] Incoming webhook. adding labels %s to %s#%s', chalk.yellow('forum-reminder'), JSON.stringify(labels.pr), repo, data.pull_request.number);
-	var opts = {
-		method:'POST',
-		uri: data.pull_request.issue_url + '/labels', // repo/labels?
-		headers: {
-			'User-Agent': 'forum-reminder',
-			'Authorization': 'token '+accessToken,
-			'Content-Type': 'application/json'
-		},
-		form: JSON.stringify(labels.pr)
-	};
-	request(opts, function(err, results, body){
-		if (err) {console.error(err);}
-		debug('[%s] API response %s', chalk.magenta('github'), JSON.stringify(body, null, ' '));
-	});
-});
+/**
+ *
+ * @param repo
+ * @param data
+ * @returns Promise
+ */
+function handleClosedIssue(repo, data) {
+	// Check if issue is closed
+    //if (data.action !== 'opened') {return;}
+    // GET comments from issue
+    rp.get({
+        uri: data.issue.url + '/comments',
+        headers: {
+            'User-Agent': 'forum-reminder',
+            'Authorization': 'token ' + accessToken,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function(comments) {
+        console.log(comments[0].body);
+    });
+    // // Foreach comment (GET individual comments? Or GET entire array of comments)
+    // //   If keyword found in comment -> Add user to list of users to @mention
+    // // Create message
+    // // POST comment using Github API
+    // var opts = {
+    //     method:'POST',
+    //     uri: data.issue.url + '/labels',
+    //     headers: {
+    //         'User-Agent': 'forum-reminder',
+    //         'Authorization': 'token '+accessToken,
+    //         'Content-Type': 'application/json'
+    //     },
+    //     form: ""
+    // };
+    // request(opts, function(err, results, body){ // request-promise?
+    //     if (err) {console.error(err);}
+    //     debug('[%s] API response %s', chalk.magenta('github'), JSON.stringify(body, null, ' '));
+    // });
+}
 
-webhookHandler.on('issues', function (repo, data) {
-	if (repos.indexOf(repo) < 0 || data.action !== 'opened' || labels.issue.length === 0) {return;}
-	debug('[%s] Incoming webhook. adding labels %s to %s#%s', chalk.yellow('forum-reminder'), JSON.stringify(labels.issue), repo, data.issue.number);
-	var opts = {
-		method:'POST',
-		uri: data.issue.url + '/labels',
-		headers: {
-			'User-Agent': 'forum-reminder',
-			'Authorization': 'token '+accessToken,
-			'Content-Type': 'application/json'
-		},
-		form: JSON.stringify(labels.issue)
-	};
-	request(opts, function(err, results, body){
-		if (err) {console.error(err);}
-		debug('[%s] API response %s', chalk.magenta('github'), JSON.stringify(body, null, ' '));
-	});
-});
+webhookHandler.on('issues', handleClosedIssue);
 
 webhookHandler.on('error', function (err, req, res) {
 	console.error('an error occurred', err);
 });
 
 app.listen(app.get('port'), function () {
-	console.log('Auto Labeler listening on port ' + app.get('port'));
+	console.log('Forum reminder listening on port ' + app.get('port'));
 });
