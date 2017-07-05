c1d9c20d5d5584f0a8c9621673ba9fd27ea64d24
Ottavio Hartman <omh1280@gmail.com>
2017-07-05 14:23:13 -0400

    More refactor

--- index.js
+++ index.js
@@ -1,90 +1,87 @@
 'use strict';
-var express = require('express');
-var rp = require('request-promise');
-var githubWebHook = require('express-github-webhook');
 var bodyParser = require('body-parser');
+var express = require('express');
+var gitHubWebHook = require('express-github-webhook');
+var rp = require('request-promise');
 
-//var repos = ['https://github.com/omh1280/cesium'];
+var headers = {
+    'User-Agent': 'forum-reminder',
+    'Authorization': 'token ' + process.env.GITHUB_TOKEN
+};
 
-var accessToken = process.env.GITHUB_TOKEN;
-var webhookHandler = githubWebHook({
+var webHookHandler = gitHubWebHook({
     path: '/',
-    secret: ''
+    secret: process.env.SECRET || ''
 });
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json());
-app.use(webhookHandler);
+app.use(webHookHandler);
 
-function findGoogleGroupOccurences(comments) {
-    var googleGroupOccurences = [];
-    var re = /https:\/\/groups\.google\.com[^\s]*/ig;
-    // If keyword found in comment -> Add user to list of users to @mention
+function findLinksWithRegex(comments, re) {
+    var linkMatches = [];
     for (var i = 0; i < comments.length; i++) {
-        var matches = comments[i].body.match(re);
-        if (matches && !googleGroupOccurences.includes(matches[0])) {
-            googleGroupOccurences.push(matches[0]);
+        var matchResult = comments[i].body.match(re);
+        if (matchResult && !linkMatches.includes(matchResult[0])) {
+            linkMatches.push(matchResult[0]);
         }
     }
-    console.log(googleGroupOccurences);
-    return googleGroupOccurences;
+    return linkMatches;
 }
 
-function postMessage(googleGroupOccurences, url) {
-    // Create message
-    // POST comment using Github API
-    if (googleGroupOccurences.length === 0) {
-        console.log('No google group links found in comments!');
-        return;
-    }
-    var message = 'Please make sure to update ' + googleGroupOccurences + ' on this closed issue.\n\n__I am a bot BEEEP BOOOP__';
-    var opts = {
+function postComment(message, url) {
+    return rp.post({
         uri: url,
-        headers: {
-            'User-Agent': 'forum-reminder',
-            'Authorization': 'token ' + accessToken
-        },
+        headers: headers,
         body: {
             'body': message
         },
         json: true
-    };
-    return rp.post(opts).then(function(status) {
-        console.log('GitHub API returned with: ' + status);
     });
 }
 
-function handleClosedIssue(repo, data) { //eslint-disable-line no-unused-vars
-	// Check if issue is closed
-    if (data.action !== 'closed') {
-        return;
-    }
+function handleClosedIssue(data) {
     // GET comments from issue
     rp.get({
         uri: data.issue.url + '/comments',
-        headers: {
-            'User-Agent': 'forum-reminder',
-            'Authorization': 'token ' + accessToken,
-            'Content-Type': 'application/json'
-        },
+        headers: headers,
         json: true
     })
-    .then(findGoogleGroupOccurences)
-    .then(function(googleGroupOccurences) {
-        return postMessage(googleGroupOccurences, data.issue.url + '/comments');
+    .then(function(comments) {
+        return findLinksWithRegex(comments, /https:\/\/groups\.google\.com[^\s]*/ig);
+    })
+    .then(function(linkMatches) {
+        if (linkMatches.length === 0) {
+            console.log('No google group links found in comments!');
+            return;
+        }
+        console.log(linkMatches);
+        var message = 'Please make sure to update ' + linkMatches + ' on this closed issue.\n\n__I am a bot BEEEP BOOOP__';
+
+        return postComment(message, data.issue.url + '/comments');
+    })
+    .then(function(status) {
+        console.log('GitHub API returned with: ' + status);
     })
     .catch(function(e) {
         console.log('Got an ERROR: ' + e);
     });
 }
 
-webhookHandler.on('issues', handleClosedIssue);
-
-webhookHandler.on('error', function (err, req, res) { //eslint-disable-line no-unused-vars
-	console.error('an error occurred', err);
+// Listen to `issues` WebHook
+webHookHandler.on('issues', function(repo, data) { //eslint-disable-line no-unused-vars
+    if (data.action !== 'closed') {
+        return;
+    }
+    handleClosedIssue(data);
 });
 
+webHookHandler.on('error', function (err, req, res) { //eslint-disable-line no-unused-vars
+	console.error('An error occurred: ', err);
+});
+
+// Listen to port specified by env.PORT
 app.listen(app.get('port'), function () {
 	console.log('Forum-reminder listening on port ' + app.get('port'));
 });
