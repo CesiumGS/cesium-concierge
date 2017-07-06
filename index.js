f3ecdbb64017e4d62b04fe70bc735a76a9d334f1
Ottavio Hartman <omh1280@gmail.com>
2017-07-06 11:39:05 -0400

    First spec passes

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 'use strict';
 var bodyParser = require('body-parser');
 var express = require('express');
+var GitHubServer = require('./lib/GitHubServer');
 var gitHubWebHook = require('express-github-webhook');
-var rp = require('request-promise');
 
-var headers = {
-    'User-Agent': 'forum-reminder',
-    'Authorization': 'token ' + process.env.GITHUB_TOKEN
-};
-var googleGroupRegex = /https:\/\/groups\.google\.com[^\s]*/ig;
+var gitHubServer = new GitHubServer('to-be-named', process.env.GITHUB_TOKEN);
+
 var webHookHandler = gitHubWebHook({
     path: '/',
     secret: process.env.SECRET || ''
@@ -22,50 +19,20 @@ app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(webHookHandler);
 
-function findLinksWithRegex(comments, regularExpression) {
-    var linkMatches = [];
-    for (var i = 0; i < comments.length; i++) {
-        var matchResult = comments[i].body.match(regularExpression);
-        if (matchResult && !linkMatches.includes(matchResult[0])) {
-            linkMatches.push(matchResult[0]);
-        }
-    }
-    return linkMatches;
-}
-
-function getComments(url) {
-    return rp.get({
-        uri: url,
-        headers: headers,
-        json: true
-    });
-}
-
-function postComment(url, message) {
-    return rp.post({
-        uri: url,
-        headers: headers,
-        body: {
-            'body': message
-        },
-        json: true
-    });
-}
-
 function handleClosedIssue(data) {
     var commentsUrl = data.issue.url + '/comments';
 
     // Return big Promise chain
-    return getComments(commentsUrl)
+    return gitHubServer.getComments(commentsUrl)
     .then(function(comments) {
-        var linkMatches = findLinksWithRegex(comments, googleGroupRegex);
+        var linkMatches = GitHubServer.findLinksWithRegex(comments);
         if (linkMatches.length === 0) {
             console.log('No google group links found in comments!');
             return;
         }
         console.log('Found these links in the comments: ', linkMatches);
         var message = 'Please make sure to update ' + linkMatches + ' on this closed issue.\n\n__I am a bot BEEEP BOOOP__';
-        return postComment(commentsUrl, message);
+        return gitHubServer.postComment(commentsUrl, message);
     })
     .then(function(status) {
         console.log('GitHub API returned with: ' + status);
--- specs/lib/GitHubServerSpec.js
+++ specs/lib/GitHubServerSpec.js
@@ -1,7 +1,8 @@
 'use strict';
-var app = require('../index');
-var regexTest = app.findLinksWithRegex;
-var googleGroupRegex = app.googleGroupRegex;
+var GitHubServer = require('../../lib/GitHubServer');
+
+var regexTest = GitHubServer.findLinksWithRegex;
+var googleGroupRegex = GitHubServer.googleGroupRegex;
 
 describe('Regex work correctly', function() {
     var comments = [];
@@ -13,10 +14,10 @@ describe('Regex work correctly', function() {
             { body: 'http://groups.google.com/'},
             { body: 'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/x02lygB7hYc' },
             { body: 'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/x02lygB7hYc' }
-        ]
+        ];
     });
 
     it('Finds correct number of unique links', function() {
-        expect(regexTest(comments, googleGroupRegex).length).toEqual(3);
+        expect(regexTest(comments).length).toEqual(3);
     });
 });
