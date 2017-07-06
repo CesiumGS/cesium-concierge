5f2a1beb51245a5c4fba4a328f5249e49524438b
Ottavio Hartman <omh1280@gmail.com>
2017-07-06 14:43:53 -0400

    Refactor

--- index.js
+++ index.js
@@ -35,17 +35,31 @@ app.set('port', nconf.get('port') || 5000);
 app.use(bodyParser.json());
 app.use(webHookHandler);
 
+var gitHubServer = new GitHubServer('to-be-named', nconf.get('github_token'));
+
+/** Get Issue title + body -> parse for keywords -> post labels + comment
+ *
+ * @param {Object} data Generic JSON object passed from the GitHub REST API (https://developer.github.com/v3/activity/events/types/)
+ */
+function handleOpenedIssue(data) {
+    var issueUrl = data.issue.url;
+
+    return gitHubServer.get(issueUrl)
+    .then(function() {
+        // TODO
+    });
+}
+
 /** Get comments -> regex search -> post comment
  *
  * @param {Object} data Generic JSON object passed from the GitHub REST API (https://developer.github.com/v3/activity/events/types/#issuesevent)
  */
 function handleClosedIssue(data) {
-    var gitHubServer = new GitHubServer('to-be-named', nconf.get('github_token'));
     var commentsUrl = data.issue.url + '/comments';
 
-    return gitHubServer.getComments(commentsUrl)
-    .then(function(comments) {
-        var linkMatches = GitHubServer.findLinksWithRegex(comments);
+    return gitHubServer.get(commentsUrl)
+    .then(function(jsonResponse) {
+        var linkMatches = GitHubServer.findLinksWithRegex(jsonResponse.body);
         if (linkMatches.length === 0) {
             console.log('No google group links found in comments!');
             return;
@@ -62,12 +76,34 @@ function handleClosedIssue(data) {
     });
 }
 
-// Listen to `issues` WebHook
+/** Get PR title + body -> parse for keywords -> post labels + comment
+ *
+ * @param {Object} data Generic JSON object passed from the GitHub REST API (https://developer.github.com/v3/activity/events/types/)
+ */
+function handleOpenedPullRequest(data) {
+    var pullRequestUrl = data.pull_request.url;
+
+    return gitHubServer.get(pullRequestUrl)
+    .then(function(jsonResponse) {
+        // TODO
+    });
+}
+
+// Listen to `Issues` Event
 webHookHandler.on('issues', function(repo, data) { //eslint-disable-line no-unused-vars
-    if (data.action !== 'closed') {
+    if (data.action === 'opened') {
+        handleOpenedIssue(data);
+    } else if (data.action === 'closed') {
+        handleClosedIssue(data);
+    }
+});
+
+// Listen to `PullRequests` Event
+webHookHandler.on('pull_request', function (repo, data) { // eslint-disable-line no-unused-vars
+    if (data.action !== 'opened') {
         return;
     }
-    handleClosedIssue(data);
+    handleOpenedPullRequest(data);
 });
 
 webHookHandler.on('error', function (err, req, res) { //eslint-disable-line no-unused-vars
@@ -76,5 +112,5 @@ webHookHandler.on('error', function (err, req, res) { //eslint-disable-line no-u
 
 // Start server on port specified by env.PORT
 app.listen(app.get('port'), function () {
-	console.log('Forum-reminder listening on port ' + app.get('port'));
+	console.log('~to-be-named~ listening on port ' + app.get('port'));
 });
--- specs/lib/GitHubServerSpec.js
+++ specs/lib/GitHubServerSpec.js
@@ -4,7 +4,7 @@ var rp = require('request-promise');
 
 var findLinksWithRegex = GitHubServer.findLinksWithRegex;
 
-describe('postComment and getComments work as expected', function() {
+describe('postComment and get work as expected', function() {
     var server = new GitHubServer('agent', '1234');
 
     it('sets User-Agent and token correctly', function() {
@@ -17,8 +17,8 @@ describe('postComment and getComments work as expected', function() {
         expect(server.postComment('url')).toBe(undefined);
     });
 
-    it('getComments returns undefined if `url` is undefined', function() {
-        expect(server.getComments()).toBe(undefined);
+    it('get returns undefined if `url` is undefined', function() {
+        expect(server.get()).toBe(undefined);
     });
 
     it('request-promise receives well-formated POST for `postComment`', function() {
@@ -40,7 +40,7 @@ describe('postComment and getComments work as expected', function() {
         server.postComment('http://test', 'hello');
     });
 
-    it('request-promise receives well-formated GET for `getComments`', function() {
+    it('request-promise receives well-formated GET for `get`', function() {
         var fakeUrl = 'http://test';
         var expected = {
             uri: fakeUrl,
@@ -52,7 +52,7 @@ describe('postComment and getComments work as expected', function() {
             expect(getJson).toEqual(expected);
         });
 
-        server.getComments('http://test');
+        server.get('http://test');
     });
 });
 
@@ -60,14 +60,13 @@ describe('Regex work correctly', function() {
     var comments = [];
     beforeEach(function() {
         comments = [
-            { body: '' },
-            { body: 'this has no google link' },
-            { body: 'https://groups.google.com/' },
-            { body: 'http://groups.google.com/'},
-            { body: 'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/x02lygB7hYc' },
-            { body: 'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/x02lygB7hYc' },
-            { body: 'Reported here: https://groups.google.com/forum/?hl=en#!topic/cesium-dev/fewafjdsk\n\nSeen with Chrome 59.0.3071.102. iOS version: 10.3.2. Perhaps a driver issue?\n' }
-        ];
+            '',
+            'this has no google link',
+            'https://groups.google.com/',
+            'http://groups.google.com/',
+            'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/x02lygB7hYc',
+            'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/x02lygB7hYc',
+            'Reported here: https://groups.google.com/forum/?hl=en#!topic/cesium-dev/fewafjdsk\n\nSeen with Chrome 59.0.3071.102. iOS version: 10.3.2. Perhaps a driver issue?\n'];
     });
 
     it('Finds correct number of unique links', function() {
@@ -75,9 +74,9 @@ describe('Regex work correctly', function() {
     });
 
     it('Returns links intact', function() {
-        expect(findLinksWithRegex([comments[2]])).toEqual([comments[2].body]);
-        expect(findLinksWithRegex([comments[3]])).toEqual([comments[3].body]);
-        expect(findLinksWithRegex([comments[4]])).toEqual([comments[4].body]);
+        expect(findLinksWithRegex([comments[2]])).toEqual([comments[2]]);
+        expect(findLinksWithRegex([comments[3]])).toEqual([comments[3]]);
+        expect(findLinksWithRegex([comments[4]])).toEqual([comments[4]]);
     });
 
     it('Finds link when surrounded by text', function() {
