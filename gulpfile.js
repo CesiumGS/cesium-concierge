287a51984b4c5edfa098bd6efe70384222340189
Ottavio Hartman <omh1280@gmail.com>
2017-07-12 09:37:37 -0400

    Fix coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var Cesium = require('cesium');
+var child_process = require('child_process');
+var fsExtra = require('fs-extra');
 var gulp = require('gulp');
 var Jasmine = require('jasmine');
 var JasmineSpecReporter = require('jasmine-spec-reporter');
+var open = require('open');
 var yargs = require('yargs');
 
 var defined = Cesium.defined;
@@ -19,4 +22,21 @@ gulp.task('test', function (done) {
     jasmine.onComplete(function (passed) {
         done(argv.failTaskOnError && !passed ? 1 : 0);
     });
+});
+
+gulp.task('coverage', function() {
+    fsExtra.removeSync('coverage');
+
+    child_process.execSync('nyc' +
+        ' --all' +
+        ' --report-dir coverage/' +
+        ' -x "specs/**"' +
+        ' -x "coverage/**"' +
+        ' -x "gulpfile.js"' +
+        ' --reporter=lcov' +
+        ' node_modules/jasmine/bin/jasmine.js' +
+        ' JASMINE_CONFIG_PATH=specs/jasmine.json', {
+        stdio: [process.stdin, process.stdout, process.stderr]
+    });
+    open('coverage/lcov-report/index.html');
 });
