31191bfe77fa3116aa4c4eb44fc956b24532b379
Ottavio Hartman <omh1280@gmail.com>
2017-07-13 16:29:32 -0400

    Use Promises instead of throw/catch

--- index.js
+++ index.js
@@ -9,41 +9,39 @@ var Settings = require('./lib/Settings');
 var app = express();
 module.exports = app;
 
-try {
-    Settings.loadRepositoriesSettings();
-} catch (err) {
+Settings.loadRepositoriesSettings().then(function() {
+    var webHookHandler = gitHubWebHook({
+        path: Settings.listenPath,
+        secret: Settings.secret
+    });
+
+    app.use(bodyParser.json());
+    app.use(webHookHandler);
+
+    Settings.repositories.forEach(function(repositoryName) {
+        webHookHandler.on(repositoryName, function (event, jsonResponse) {
+            if (Settings.get(repositoryName, 'remindForum') && event === 'issues' && jsonResponse.data === 'closed') {
+                commentOnClosedIssue(jsonResponse, {
+                    'User-Agent': 'cesium-concierge',
+                    Authorization: 'token ' + Settings.get(repositoryName, 'gitHubToken')
+                }).then(function (status) {
+                    console.log('GitHub API returned with:', status);
+                }).catch(function (e) {
+                    console.log('commentOnClosedIssue got an error:', e);
+                });
+            }
+        });
+    });
+
+    webHookHandler.on('error', function (err, req, res) { // eslint-disable-line no-unused-vars
+        console.log('WebHookHandler got error:', err);
+    });
+
+    // Start server on port specified by env.PORT
+    app.listen(Settings.port, function () {
+        console.log('cesium-concierge listening on port', Settings.port);
+    });
+}).catch(function(err) {
     console.log('Could not parse `repository`.json:', err);
     process.exit(1);
-}
-
-var webHookHandler = gitHubWebHook({
-    path: Settings.listenPath,
-    secret: Settings.secret
-});
-
-app.use(bodyParser.json());
-app.use(webHookHandler);
-
-Settings.repositories.forEach(function(repositoryName) {
-    webHookHandler.on(repositoryName, function (event, jsonResponse) {
-        if (Settings.get(repositoryName, 'remindForum') && event === 'issues' && jsonResponse.data === 'closed') {
-            commentOnClosedIssue(jsonResponse, {
-                'User-Agent': 'cesium-concierge',
-                Authorization: 'token ' + Settings.get(repositoryName, 'gitHubToken')
-            }).then(function (status) {
-                console.log('GitHub API returned with:', status);
-            }).catch(function (e) {
-                console.log('commentOnClosedIssue got an error:', e);
-            });
-        }
-    });
-});
-
-webHookHandler.on('error', function (err, req, res) { // eslint-disable-line no-unused-vars
-	console.log('WebHookHandler got error:', err);
-});
-
-// Start server on port specified by env.PORT
-app.listen(Settings.port, function () {
-	console.log('cesium-concierge listening on port', Settings.port);
 });
