2805dafb717c1a9781086744fe61ffc1b0532708
Ottavio Hartman <omh1280@gmail.com>
2017-07-18 10:03:15 -0400

    Improve logging, check response status code

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 'use strict';
+var Cesium = require('cesium');
 var bodyParser = require('body-parser');
 var express = require('express');
 var gitHubWebHook = require('express-github-webhook');
+var Promise = require('bluebird');
+
+var defined = Cesium.defined;
 
 var commentOnClosedIssue = require('./lib/commentOnClosedIssue');
 var Settings = require('./lib/Settings');
 
 Settings.loadRepositoriesSettings('./config.json')
 .then(function (repositoryNames) {
+    console.log(new Date(Date.now()).toISOString() + ' Loaded settings successfully');
     var webHookHandler = gitHubWebHook({
         path: Settings.listenPath,
         secret: Settings.secret
@@ -23,18 +28,25 @@ Settings.loadRepositoriesSettings('./config.json')
             console.log(new Date(Date.now()).toISOString() + ' Received event to repository:', repositoryName);
             console.log(new Date(Date.now()).toISOString() + ' event:', event);
             console.log(new Date(Date.now()).toISOString() + ' jsonResponse:', jsonResponse);
-            //var repository = Settings.repositories[repositoryName];
-            if (event === 'issues' &&
-                jsonResponse.action === 'closed') {
-                commentOnClosedIssue(jsonResponse, {
-                    'User-Agent': 'cesium-concierge',
-                    Authorization: 'token ' + Settings.repositories[repositoryName].gitHubToken
-                }).then(function (status) {
-                    console.log(new Date(Date.now()).toISOString() + ' GitHub API returned with:', status);
-                }).catch(function (e) {
-                    console.log(new Date(Date.now()).toISOString() + ' commentOnClosedIssue got an error:', e);
+
+            var promise = Promise.resolve();
+            if (event === 'issues' && jsonResponse.action === 'closed') {
+                promise = promise.then(function () {
+                    return commentOnClosedIssue(jsonResponse, {
+                        'User-Agent': 'cesium-concierge',
+                        Authorization: 'token ' + Settings.repositories[repositoryName].gitHubToken
+                    });
                 });
             }
+            promise.then(function (res) {
+                if (!defined(res)) {
+                    console.log(new Date(Date.now()).toISOString() + ' GitHub request did not match any events the server is listening for');
+                }
+                console.log(new Date(Date.now()).toISOString() + ' GitHub API returned with statusCode:', res.statusCode);
+                console.log(new Date(Date.now()).toISOString() + ' GitHub API returned with statusMessage:', res.statusMessage);
+            }).catch(function (e) {
+                console.log(new Date(Date.now()).toISOString() + ' commentOnClosedIssue got an error:', e);
+            });
         });
     });
 
@@ -48,6 +60,6 @@ Settings.loadRepositoriesSettings('./config.json')
     });
 })
 .catch(function (err) {
-    console.log('Could not parse environment settings:', err);
+    console.log(new Date(Date.now()).toISOString() + ' Could not parse environment settings:', err);
     process.exit(1);
 });
