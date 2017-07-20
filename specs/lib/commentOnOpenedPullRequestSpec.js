54e5c5e26f0e00db49d7eefb9ecf7ccbca2eea71
Ottavio Hartman <omh1280@gmail.com>
2017-07-20 10:13:57 -0400

    Full coverage with tests

--- specs/lib/commentOnOpenedPullRequestSpec.js
+++ specs/lib/commentOnOpenedPullRequestSpec.js
@@ -6,26 +6,178 @@ var requestPromise = require('request-promise');
 
 var commentOnOpenedPullRequest = require('../../lib/commentOnOpenedPullRequest');
 
+describe('commentOnOpenedPullRequest', function () {
+    it('throws if `jsonResponse` is undefined', function () {
+        expect(function () {
+            commentOnOpenedPullRequest(undefined);
+        }).toThrowError();
+    });
+
+    it('throws if `headers` is undefined', function () {
+        expect(function () {
+            commentOnOpenedPullRequest({}, undefined);
+        }).toThrowError();
+    });
+
+    it('returns rejected promise if json is not a pull request event', function (done) {
+        var issueJson = fsExtra.readJsonSync('./specs/data/events/issue.json');
+        commentOnOpenedPullRequest(issueJson, {})
+            .then(function () {
+                done.fail();
+            })
+            .catch(function (err) {
+                if (/pull_request/.test(err)) {
+                    return done();
+                }
+                done.fail();
+            });
+    });
+
+    it('gets correct URLs from JSON response', function () {
+        spyOn(commentOnOpenedPullRequest, '_implementation');
+        var pullRequestJson = fsExtra.readJsonSync('./specs/data/events/pullRequest.json');
+        commentOnOpenedPullRequest(pullRequestJson, {});
+        expect(commentOnOpenedPullRequest._implementation).toHaveBeenCalledWith('https://api.github.com/repos/baxterthehacker/public-repo/pulls/1/files',
+            'https://api.github.com/repos/baxterthehacker/public-repo/issues/1/comments', {}, undefined, undefined, undefined);
+
+        commentOnOpenedPullRequest(pullRequestJson, {}, 'a', 'b', ['c']);
+        expect(commentOnOpenedPullRequest._implementation).toHaveBeenCalledWith('https://api.github.com/repos/baxterthehacker/public-repo/pulls/1/files',
+            'https://api.github.com/repos/baxterthehacker/public-repo/issues/1/comments', {}, 'a', 'b', ['c']);
+    });
+});
+
+describe('commentOnOpenedPullRequest._implementation', function () {
+    var pullRequestFiles = fsExtra.readJsonSync('./specs/data/responses/pullRequestFiles.json');
+    var pullRequestFiles404 = fsExtra.readJsonSync('./specs/data/responses/pullRequestFiles_404.json');
+    var pullRequestFilesWithChanges = fsExtra.readJsonSync('./specs/data/responses/pullRequestFiles_CHANGES.json');
+
+    function okPullRequest() {
+        spyOn(requestPromise, 'get').and.returnValue(Promise.resolve(pullRequestFiles));
+    }
+    beforeEach(function () {
+        spyOn(requestPromise, 'post');
+    });
+
+    it('rejects 404 code', function (done) {
+        spyOn(requestPromise, 'get').and.returnValue(Promise.resolve(pullRequestFiles404));
+        commentOnOpenedPullRequest._implementation()
+            .then(function () {
+                done.fail();
+            })
+            .catch(function (err) {
+                if (/status code/.test(err)) {
+                    return done();
+                }
+                done.fail();
+            });
+    });
+
+    it('gets files from JSON', function (done) {
+        okPullRequest();
+        spyOn(commentOnOpenedPullRequest, '_didUpdateChanges');
+        spyOn(commentOnOpenedPullRequest, '_didUpdateThirdParty');
+        commentOnOpenedPullRequest._implementation()
+            .then(function () {
+                expect(commentOnOpenedPullRequest._didUpdateChanges).toHaveBeenCalledWith(['.gitignore',
+                    'index.js', 'lib/Settings.js', 'lib/commentOnClosedIssue.js', 'lib/getUniqueMatch.js',
+                    'specs/data/config_noError.json', 'specs/data/config_noGitHubToken.json',
+                    'specs/data/config_noRepositories.json', 'specs/data/config_noRepositoryNames.json',
+                    'specs/data/config_noSecret.json', 'specs/lib/SettingsSpec.js']);
+                expect(commentOnOpenedPullRequest._didUpdateThirdParty).toHaveBeenCalledWith(['.gitignore',
+                    'index.js', 'lib/Settings.js', 'lib/commentOnClosedIssue.js', 'lib/getUniqueMatch.js',
+                    'specs/data/config_noError.json', 'specs/data/config_noGitHubToken.json',
+                    'specs/data/config_noRepositories.json', 'specs/data/config_noRepositoryNames.json',
+                    'specs/data/config_noSecret.json', 'specs/lib/SettingsSpec.js'], undefined);
+                done();
+            })
+            .catch(function (err) {
+                done.fail(err);
+            });
+    });
+
+    it('Posts Third Party signature', function (done) {
+        okPullRequest();
+        commentOnOpenedPullRequest._implementation('', '', {}, 'CHANGES_SIGNATURE', 'THIRD_PARTY_SIGNATURE', ['/specs/data/'])
+            .then(function () {
+                var obj = requestPromise.post.calls.argsFor(0)[0];
+                console.log(obj);
+                expect(/THIRD_PARTY/.test(obj.body.body)).toBe(true);
+                expect(/CHANGES/.test(obj.body.body)).toBe(false);
+                done();
+            })
+            .catch(function (err) {
+                done.fail(err);
+            });
+    });
+
+    it('Posts CHANGES signature', function (done) {
+        spyOn(requestPromise, 'get').and.returnValue(Promise.resolve(pullRequestFilesWithChanges));
+        commentOnOpenedPullRequest._implementation('', '', {}, 'CHANGES_SIGNATURE', 'THIRD_PARTY_SIGNATURE', ['/some/folder'])
+            .then(function () {
+                var obj = requestPromise.post.calls.argsFor(0)[0];
+                console.log(obj);
+                expect(/THIRD_PARTY/.test(obj.body.body)).toBe(false);
+                expect(/CHANGES/.test(obj.body.body)).toBe(true);
+                done();
+            })
+            .catch(function (err) {
+                done.fail(err);
+            });
+    });
+});
+
 describe('commentOnOpenedPullRequest._didUpdateChanges', function () {
     it('returns false for []', function () {
-        expect(commentOnOpenedPullRequest._didUpdateChanges([])).toEqual(false);
+        expect(commentOnOpenedPullRequest._didUpdateChanges([])).toBe(false);
     });
 
     it('returns true for CHANGES.md at top level', function () {
-        expect(commentOnOpenedPullRequest._didUpdateChanges(['CHANGES.md'])).toEqual(true);
-        expect(commentOnOpenedPullRequest._didUpdateChanges(['/test/test.txt', '/a/b/c', 'CHANGES.md'])).toEqual(true);
-        expect(commentOnOpenedPullRequest._didUpdateChanges(['/a', './CHANGES.md', 'b'])).toEqual(true);
+        expect(commentOnOpenedPullRequest._didUpdateChanges(['CHANGES.md'])).toBe(true);
+        expect(commentOnOpenedPullRequest._didUpdateChanges(['/test/test.txt', '/a/b/c', 'CHANGES.md'])).toBe(true);
+        expect(commentOnOpenedPullRequest._didUpdateChanges(['/a', './CHANGES.md', 'b'])).toBe(true);
     });
 
     it('returns false for closely-named CHANGES.md files', function () {
-        expect(commentOnOpenedPullRequest._didUpdateChanges(['CHANGES.txt'])).toEqual(false);
-        expect(commentOnOpenedPullRequest._didUpdateChanges(['CHANGES.old.md'])).toEqual(false);
-        expect(commentOnOpenedPullRequest._didUpdateChanges(['.CHANGES.md'])).toEqual(false);
-        expect(commentOnOpenedPullRequest._didUpdateChanges(['/CHANGES'])).toEqual(false);
+        expect(commentOnOpenedPullRequest._didUpdateChanges(['CHANGES.txt'])).toBe(false);
+        expect(commentOnOpenedPullRequest._didUpdateChanges(['CHANGES.old.md'])).toBe(false);
+        expect(commentOnOpenedPullRequest._didUpdateChanges(['.CHANGES.md'])).toBe(false);
+        expect(commentOnOpenedPullRequest._didUpdateChanges(['/CHANGES'])).toBe(false);
     });
 
     it('returns false for CHANGES.md at other levels', function () {
-        expect(commentOnOpenedPullRequest._didUpdateChanges(['./a/CHANGES.md', './b/CHANGES.md'])).toEqual(false);
-        expect(commentOnOpenedPullRequest._didUpdateChanges(['/a/b/c/CHANGES.md'])).toEqual(false);
+        expect(commentOnOpenedPullRequest._didUpdateChanges(['./a/CHANGES.md', './b/CHANGES.md'])).toBe(false);
+        expect(commentOnOpenedPullRequest._didUpdateChanges(['/a/b/c/CHANGES.md'])).toBe(false);
+    });
+});
+
+describe('commentOnOpenedPullRequest._didUpdateThirdParty', function () {
+    it('returns false when thirdPartyFolders is []', function () {
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['file.txt'], [])).toBe(false);
+    });
+
+    it('matches file with list of folders', function () {
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['/a/b/file.txt'], ['/some/folder', '/a/b'])).toBe(true);
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['.gitignore',
+            'index.js', 'lib/Settings.js', 'lib/commentOnClosedIssue.js', 'lib/getUniqueMatch.js',
+            'specs/data/config_noError.json', 'specs/data/config_noGitHubToken.json',
+            'specs/data/config_noRepositories.json', 'specs/data/config_noRepositoryNames.json',
+            'specs/data/config_noSecret.json', 'specs/lib/SettingsSpec.js'], 'lib')).toBe(true);
+    });
+
+    it('matches multiple files with list of folders', function () {
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['a/b/file.txt', ''], ['/some/folder/', '/a/b/'])).toBe(true);
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['/file.txt'], ['/', '/a/b/'])).toBe(true);
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['./b/file.txt'], ['/some/folder/', './b/'])).toBe(true);
+    });
+
+    it('does not confuse files with folders', function () {
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['c.txt'], ['/some/folder/', '/a/b/', '/c/'])).toBe(false);
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['./c.txt'], ['/some/folder/', '/a/b/', '/c/npm/'])).toBe(false);
+    });
+
+    it('returns false for non-matching files', function () {
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['c.txt'], ['/some/folder/'])).toBe(false);
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['c.txt'], ['/a/b/c/'])).toBe(false);
+        expect(commentOnOpenedPullRequest._didUpdateThirdParty(['/a/bc.txt'], ['/a/b/c/'])).toBe(false);
     });
 });
