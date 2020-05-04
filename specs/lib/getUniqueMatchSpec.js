c8f1821c18bcdcff6f98019b50e38988f175d3b2
hpinkos <hannah@cesium.com>
2020-05-04 09:37:52 -0400

    remove check for google groups links

--- specs/lib/getUniqueMatchSpec.js
+++ specs/lib/getUniqueMatchSpec.js
@@ -3,8 +3,6 @@
 var commentOnClosedIssue = require('../../lib/commentOnClosedIssue');
 var getUniqueMatch = require('../../lib/getUniqueMatch');
 
-var googleLinkRegex = commentOnClosedIssue._googleLinkRegex;
-
 describe('getUniqueMatch', function () {
     var t1 = ['a', 'b', 'c', 'd'];
     var t2 = ['a', 'a', 'b', 'bb', 'c', 'aaa', 'cab', 'c'];
@@ -23,58 +21,5 @@ describe('getUniqueMatch', function () {
     it('finds unique matches', function () {
         expect(getUniqueMatch(t1, /[a-z]/g)).toEqual(t1);
         expect(getUniqueMatch(t2, /[a-z]/g)).toEqual(['a', 'b', 'c']);
-    });
-});
-
-describe('Google Group regex', function () {
-    var empty = '';
-    var noLink = 'this has no google link';
-    var sslLink = 'https://groups.google.com/forum/#!topic/cesium-dev/Wd0P0EnFDjA';
-    var noSslLink = 'http://groups.google.com/forum/#!topic/cesium-dev/Wd0P0EnFDjA';
-    var surroundedText = 'Reported here: https://groups.google.com/forum/?hl=en#!topic/cesium-dev/fewafjdsk\n\nSeen with Chrome 59.0.3071.102. iOS version: 10.3.2. Perhaps a driver issue?\n';
-    var commaAtEnd = 'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test1, fdsfoewjaf fjdsa f';
-    var twoLinks = 'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test2, https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test5';
-    var twoSameLinks = 'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test2, https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test2, https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test3';
-    var markdownLink = 'This fixes #5446, also see this [forum post.](https://groups.google.com/forum/#!msg/cesium-dev/Ktn8aPQmOsQ/_Dbd7igkCQAJ)\\r\\n\\r\\nThis adds';
-    var returnAfterLink = 'https://groups.google.com/forum/#!msg/cesium-dev/Ktn8aPQmOsQ/_Dbd7igkCQAJ\\r\\n';
-    var baseLink = 'https://groups.google.com/forum/!forum/cesium-dev';
-    var baseLink2 = 'https://groups.google.com/forum/?hl=en#!forum/cesium-dev';
-
-    it('Returns empty array for ""', function () {
-        expect(getUniqueMatch([empty], googleLinkRegex)).toEqual([]);
-    });
-
-    it('Returns empty array if `textArray` is undefined', function () {
-        expect(getUniqueMatch(undefined)).toEqual([]);
-    });
-
-    it('Returns empty array if `regex` is undefined', function () {
-        expect(getUniqueMatch(['test'], undefined)).toEqual([]);
-    });
-
-    it('Finds correct number of unique links', function () {
-        expect(getUniqueMatch([noLink], googleLinkRegex)).toEqual([]);
-        expect(getUniqueMatch([sslLink], googleLinkRegex)).toEqual([sslLink]);
-        expect(getUniqueMatch([sslLink, sslLink], googleLinkRegex)).toEqual([sslLink]);
-        expect(getUniqueMatch([noSslLink, noSslLink], googleLinkRegex)).toEqual([noSslLink]);
-    });
-
-    it('Finds links when surrounded by text', function () {
-        expect(getUniqueMatch([surroundedText, surroundedText], googleLinkRegex)).toEqual(['https://groups.google.com/forum/?hl=en#!topic/cesium-dev/fewafjdsk']);
-        expect(getUniqueMatch([commaAtEnd], googleLinkRegex)).toEqual(['https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test1']);
-        expect(getUniqueMatch([twoLinks], googleLinkRegex)).toEqual(['https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test2', 'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test5']);
-        expect(getUniqueMatch([twoSameLinks], googleLinkRegex)).toEqual(['https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test2', 'https://groups.google.com/forum/?hl=en#!topic/cesium-dev/test3']);
-    });
-
-    it('Finds markdown link', function () {
-        expect(getUniqueMatch([markdownLink], googleLinkRegex)).toEqual(['https://groups.google.com/forum/#!msg/cesium-dev/Ktn8aPQmOsQ/_Dbd7igkCQAJ']);
-    });
-
-    it('Finds link with newline after', function () {
-        expect(getUniqueMatch([returnAfterLink], googleLinkRegex)).toEqual(['https://groups.google.com/forum/#!msg/cesium-dev/Ktn8aPQmOsQ/_Dbd7igkCQAJ']);
-    });
-
-    it('Doesn\'t match links to base forum', function () {
-        expect(getUniqueMatch([baseLink, baseLink2], googleLinkRegex)).toEqual([]);
     });
 });
